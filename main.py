a8c443fff3458c90f6865780e8a7b3285fd67fa4
Samer98 <samerhisham.work@gmail.com>
2023-09-13 14:39:18 +0300

    api working

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import openai
 
-openai_key = "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"
+openai_key = "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"
 openai.api_key = openai_key
 
 app = FastAPI()
@@ -15,17 +15,31 @@ seasons  = [ "summer", "winter", "spring", "autumn"]
 @app.get("/travel")
 async def get_activity(country: str , season: str):
     season = season.lower()
-    response = openai.ChatCompletion.create(
-    model = 'gpt-3.5-turbo',
-    messages = {"role": "user", "content": f"What the activity/recommendations in {country} when the season {season} is in it?"}
-    )
-    if season in seasons:
-        return {"country": country , "season": season}
-    else:
+
+    if season not in seasons:
         return {"Error":"Invalid season"}
 
-response = openai.ChatCompletion.create(
-model = 'gpt-3.5-turbo',
-message = {"role": "user", "content": "What are the activity"}
-)
-print(response)
+    response = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo',
+        messages = [{"role": "user", "content": f"What the activity/recommendations in {country} when the season {season} is in it? write the recommendation without introduction"}]
+        )
+    recommendations = response['choices'][0]['message']['content']
+    recommendations = recommendations.splitlines()
+    for space in recommendations:
+        if space == "":
+            recommendations.remove("")
+    result = {"country":country,"season":season,"recommendations":recommendations}
+    return result
+
+
+
+# response = openai.ChatCompletion.create(
+# model = 'gpt-3.5-turbo',
+# messages = [{"role": "user", "content": "What the activity/recommendations in Egypt when the season winter is in it? write the recommendation without introduction"}]
+# )
+# recommendations = response['choices'][0]['message']['content']
+# recommendations = recommendations.splitlines()
+# for space in recommendations:
+#     if space == "":
+#         recommendations.remove("")
+# print(recommendations)
