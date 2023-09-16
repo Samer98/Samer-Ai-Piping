bc0321f31408e9be6ee977dfa7f508616fbf5b32
Samer98 <samerhisham.work@gmail.com>
2023-09-16 15:44:36 +0300

    adding helper functions

---
+++ helper_functions.py
@@ -0,0 +1,52 @@
+import openai
+import os
+from country_cities import country_and_cities, seasons
+
+
+from dotenv import load_dotenv
+
+def configure():
+    load_dotenv()
+
+configure()
+
+
+def  openAi_key_validation(openai):
+    error_message = []
+    if openai.api_key is None:
+        error_message.append(f"Error occurred in openai API KEY is not found ")
+
+    return error_message
+def chatGPT_response(country, season):
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[{"role": "user",
+                   "content": f"What the activity/recommendations in {country} when the season {season} is in it? "
+                              f"write the recommendation without introduction and give 3 recommendation only"}]
+    )
+    # This line for getting the response text from ChatGPT
+    recommendations = response['choices'][0]['message']['content']
+
+    # This for splitting the text into lines to make them in list
+    recommendations = recommendations.splitlines()
+
+    # Loop for removing the empty ""
+    for space in recommendations:
+        if space == "":
+            recommendations.remove("")
+
+    context = {"country": country, 'season': season, "recommendations": recommendations}
+    return context
+
+
+def country_and_season_validation(country, season):
+    error_message = []
+
+
+    if country not in country_and_cities:
+        error_message.append(
+            "Error occurred in country or city name, Please select valid Country or City ")
+    if season not in seasons:
+        error_message.append(f"Error occurred in season name, Please select season from {seasons} ")
+
+    return error_message
--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 from fastapi import FastAPI, HTTPException, Request, Response, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import openai
-from country_cities import country_and_cities, seasons
+# import openai
 from pathlib import Path
-from dotenv import load_dotenv
+from helper_functions import chatGPT_response, country_and_season_validation , openAi_key_validation
+from country_cities import country_and_cities, seasons
 import os
+import openai
 
 
 # Function used for getting environment variables
-def configure():
-    load_dotenv()
+openai.api_key = os.getenv("openai_key")
 
 
-configure()
 
 # Api key for openAI
-openai.api_key = os.getenv("openai_key")
+# openai.api_key = os.getenv("openai_key")
 
 app = FastAPI()
 
@@ -45,65 +44,67 @@ async def get_recommendations_page(request: Request):
     return templates.TemplateResponse("recommendations.html", context)
 
 
-@app.get("/recommendations", status_code=201)
-def get_recommendations(request: Request, country: str, season: str):
+@app.get("/recommendations", status_code=200)
+def get_recommendations(request: Request, country: str, season: str,response: Response):
     data = {}
     season = season.lower()
     country = country.lower()
-
     country_and_cities.sort()
 
-    valid_country_and_cities = country in country_and_cities
-    valid_season = season in seasons
-
-    error_message = ""
-
-    if openai.api_key is None:
-        error_message += f"Error occurred in openai API KEY is not found \n "
-
-        data['country_and_cities'] = country_and_cities
-        context = {'request': request, "error_message": error_message, "data": data}
+    openAi_key = openAi_key_validation(openai)
+    if openAi_key:
+        context = {'request': request, "error_message": openAi_key, "data": data}
+        response.status_code = status.HTTP_400_BAD_REQUEST
         return templates.TemplateResponse("recommendations.html", context)
 
-    if not valid_country_and_cities:
-        error_message += "Error occurred in country or city name, Please select valid Country or City \n"
-    if not valid_season:
-        error_message += f"Error occurred in season name, Please select season from {seasons}\n "
-
-    if not (valid_country_and_cities and valid_season):
+    country_and_season_valid = country_and_season_validation(country, season)
+    if country_and_season_valid:
+        response.status_code = status.HTTP_400_BAD_REQUEST
         data['country_and_cities'] = country_and_cities
-
-        context = {'request': request, "error_message": error_message, "data": data}
-
+        context = {'request': request, "error_message": country_and_season_valid, "data": data}
         return templates.TemplateResponse("recommendations.html", context)
 
     try:
-        response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            messages=[{"role": "user",
-                       "content": f"What the activity/recommendations in {country} when the season {season} is in it? "
-                                  f"write the recommendation without introduction and give 3 recommendation only"}]
-        )
-        # This line for getting the response text from ChatGPT
-        recommendations = response['choices'][0]['message']['content']
-
-        # This for splitting the text into lines to make them in list
-        recommendations = recommendations.splitlines()
-
-        # Loop for removing the empty ""
-        for space in recommendations:
-            if space == "":
-                recommendations.remove("")
+        context = chatGPT_response(country, season)
 
         data['country'] = country
         data['season'] = season
-        data['recommendations'] = recommendations
+        data['recommendations'] = context['recommendations']
         data['country_and_cities'] = country_and_cities
         context = {'request': request, "data": data}
-        return templates.TemplateResponse("recommendations.html", context)
-
 
     except Exception as error_message:
         data['country_and_cities'] = country_and_cities
         context = {'request': request, "error_message": error_message, "data": data}
-        return templates.TemplateResponse("recommendations.html", context)
+
+    return templates.TemplateResponse("recommendations.html", context)
+
+
+
+
+@app.get("/api/recommendations", status_code=200)
+def get_recommendations(country: str, season: str,response: Response):
+    data = {}
+    season = season.lower()
+    country = country.lower()
+    country_and_cities.sort()
+
+    openAi_key = openAi_key_validation(openai)
+    if openAi_key:
+        context = {"error_message": openAi_key}
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return context
+
+    country_and_season_valid = country_and_season_validation(country, season)
+    if country_and_season_valid:
+        context = {"error_message": country_and_season_valid}
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return context
+
+    try:
+        context = chatGPT_response(country, season)
+    except Exception as error_message:
+        data['country_and_cities'] = country_and_cities
+        context = {"error_message": error_message, "data": data}
+
+    return context
